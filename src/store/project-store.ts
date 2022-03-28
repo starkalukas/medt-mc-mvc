743085caf7eba9fd2a558f2faea4dab81bc3cc54
StarkaLukas <lstarka141000@gmail.com>
2022-03-28 11:14:24 +0200

    simplify store

--- src/store/project-store.ts
+++ src/store/project-store.ts
@@ -5,44 +5,21 @@ import produce from "immer";
 
 const initialState: Model = {
     projects: [],
-    currentProjectId: null
 }
 
 class ProjectStore {
     private subject = new BehaviorSubject<Model>(initialState)
 
     set projects(projects: Project[]) {
-        this.next(produce(this.state, model => {
-            model.projects = projects.reduce((array, projects) => {
-                    array[projects.id] = projects
-                    return array
-                },
-                []);
-        }));
+        const newState = produce(this.subject.getValue(), draft => {
+            draft.projects = projects;
+        });
+
+        this.subject.next(newState);
     }
 
-    set project(project: Project) {
-        this.next(produce(this.state, model => {
-            model.projects[project.id] = project
-        }))
-    }
-    set currentProjectId(id: number) {
-        this.next(produce(this.state, model => {
-            model.currentProjectId = id
-        }));
-    }
-    set state(_notAllowed: Model) {
-        throw new Error("state is read only");
-    }
-    get state() {
-        return this.subject.getValue();
-    }
     get model(): Observable<Model> {
         return this.subject;
-    }
-
-    private next(state: Model) {
-        this.subject.next(state);
     }
 }
 
